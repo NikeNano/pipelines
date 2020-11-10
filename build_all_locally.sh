ae7f13687004a55c975f49a1cecce1a7dc846002
NikeNano <niklas.sven.hansson@gmail.com>
2020-11-10 22:14:12 +0100

    updated the local setup

--- build_all_locally.sh
+++ build_all_locally.sh
@@ -5,44 +5,46 @@ export PERSISTENCEAGENT_SERVER_TAG="persistenceagent_server"
 export SCHEDULEDWORKFLOW_SERVER_TAG="scheduledworkflow_server"
 export VIEWERCONTROLLER_SERVER_TAG="viewerconroller_server"
 export VISUALIZATION_SERVER_TAG="visualization_server"
+export DOCKER_USERNAME='nikenano'
+
 
 docker build \
       -t bazel:0.24.0 \
       -f Backend/Dockerfile.bazel .
 
 docker build \
-    -t "${API_SERVER_TAG}" \
+    -t "${DOCKER_USERNAME}"/"${API_SERVER_TAG}" \
     -f Backend/Dockerfile \
     . \
     --build-arg BAZEL_IMAGE=bazel:0.24.0
 
 
 docker build \
-    -t "${CAHCE_SERVER_TAG}" \
-    -f Backend/Dockerfile.cacheserver 
+    -t "${DOCKER_USERNAME}"/"${CAHCE_SERVER_TAG}" \
+    -f Backend/Dockerfile.cacheserver  \
     . \
     --build-arg BAZEL_IMAGE=bazel:0.24.0
 
 docker build \
-    -t "${PERSISTENCEAGENT_SERVER_TAG}" \
+    -t "${DOCKER_USERNAME}"/"${PERSISTENCEAGENT_SERVER_TAG}" \
     -f Backend/Dockerfile.persistenceagent \
     . \
     --build-arg BAZEL_IMAGE=bazel:0.24.0
 
 docker build \
-    -t "${SCHEDULEDWORKFLOW_SERVER_TAG}" \
+    -t "${DOCKER_USERNAME}"/"${SCHEDULEDWORKFLOW_SERVER_TAG}" \
     -f Backend/Dockerfile.scheduledworkflow\
     . \
     --build-arg BAZEL_IMAGE=bazel:0.24.0
 
 docker build \
-    -t "${VIEWERCONTROLLER_SERVER_TAG}" \
+    -t "${DOCKER_USERNAME}"/"${VIEWERCONTROLLER_SERVER_TAG}" \
     -f Backend/Dockerfile.viewercontroller\
     . \
     --build-arg BAZEL_IMAGE=bazel:0.24.0
 
 docker build \
-    -t "${VISUALIZATION_SERVER_TAG}" \
+    -t "${DOCKER_USERNAME}"/"${VISUALIZATION_SERVER_TAG}" \
     -f Backend/Dockerfile.visualization\
     . \
     --build-arg BAZEL_IMAGE=bazel:0.24.0
